daad4bf54ed15311d9b266d07d4867f33dbdcbc4
Todd Peterson <toddpetersongv@gmail.com>
2021-04-03 10:52:45 -0600

    Add menu component

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+        <Menu class="menu" />
+        <div class="content">
+            <ul class="categories">
+                <li class="category">Favorites</li>
+                <li class="category">Meals</li>
+                <li class="category">Cuisine</li>
+                <li class="category">Quick</li>
+                <li class="category">All</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from "./components/Menu.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {
+        Menu
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Montserrat', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Montserrat", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    font-size: 16px;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.menu {
+    background: #fcf7c9;
+    display: block;
+    height: 100vh;
+    width: 350px;
 }
 </style>
---
+++ vueapp/src/components/Menu.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <ul>
+            <li>
+                <a>Recipes</a>
+            </li>
+            <li>
+                <a>Groceries</a>
+            </li>
+            <li>
+                <a>Meals</a>
+            </li>
+            <li>
+                <a>Pantry</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    padding: 5em 2em;
+}
+
+ul {
+    list-style: none;
+}
+
+li  {
+    font-size: 1.5em;
+    margin-bottom: 0.8em;
+    cursor: pointer;
+}
+</style>
