1fed4cb3b8e94ad16de18e43fab38ecab36cb8ad
Todd Peterson <ToddPetersonGV@gmail.com>
2021-04-04 14:56:39 -0600

    Layout recipe list item

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,25 +1,49 @@
 <template>
-    <div class="container">
+    <div class="main-container">
         <Menu class="menu" />
-        <div class="content">
+        <div class="main-content">
             <ul class="categories">
-                <li class="category">Favorites</li>
+                <li class="category active">Favorites</li>
                 <li class="category">Meals</li>
                 <li class="category">Cuisine</li>
                 <li class="category">Quick</li>
                 <li class="category">All</li>
             </ul>
+            <input type="text" class="searchbox" placeholder="Search" />
+            <div class="recipe-list">
+                <RecipeListItem v-for="recipe in recipes" :key="recipe.id" :recipe="recipe"/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import Menu from "./components/Menu.vue";
+import RecipeListItem from "./components/RecipeListItem.vue";
 
 export default {
     name: "App",
     components: {
-        Menu
+        Menu,
+        RecipeListItem
+    },
+    data() {
+        return {
+            recipes: [
+                {
+                    title: "Spicy Shrimp Pilaf",
+                    description:
+                        "Use the highest quality basmati rice you can find for a perfectly fluffy and fragrant pilaf.",
+                    id: 1
+                },
+                {
+                    title: "Mushroom-Farro Soup",
+                    description:
+                        "This cozy vegetarian soup delivers all the savory richness of mushroom-barley without meat or beef broth thanks to a few umami-heavy pantry ingredients.",
+                    id: 2
+                }
+            ]
+        };
     }
 };
 </script>
@@ -43,6 +67,44 @@ export default {
     background: #fcf7c9;
     display: block;
     height: 100vh;
-    width: 350px;
+    width: 320px;
+}
+
+.main-container {
+    display: flex;
+}
+
+.main-content {
+    padding: 5em 3em;
+}
+
+.categories {
+    list-style: none;
+    display: flex;
+    margin-bottom: 1em;
+}
+
+.category {
+    font-size: 1.5em;
+    font-weight: 300;
+}
+
+.category + .category {
+    margin-left: 2em;
+}
+
+.category.active {
+    font-weight: 700;
+}
+
+.searchbox {
+    width: 36em;
+    height: 2.4em;
+    border: 1px solid black;
+    border-radius: 4px;
+    padding-left: 0.8em;
+    font-size: 1.2em;
+    font-family: "Montserrat", Helvetica, Arial, sans-serif;
+    margin-bottom: 2em;
 }
 </style>
---
+++ vueapp/src/components/RecipeListItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="content">
+            <h2 class="recipe-title">{{ recipe.title }}</h2>
+            <p class="recipe-desc">{{ recipe.description }}</p>
+            <a href="#" class="recipe-btn">See Recipe</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        recipe: Object
+    }
+};
+</script>
+
+<style scoped>
+.container {
+    margin-bottom: 3em;
+}
+
+.recipe-title {
+    margin-bottom: .7em;
+}
+
+.recipe-desc {
+    max-width: 36em;
+    margin-bottom: 1em;
+}
+
+.recipe-btn {
+    text-decoration: none;
+    background: black;
+    color: white;
+    font-size: 1.15em;
+    padding: .4em 1em;
+    font-weight: 700;
+}
+</style>
